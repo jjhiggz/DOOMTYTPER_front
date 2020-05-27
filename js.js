daf400b2e3a6e67e0f7f4a65d7d8f5be24cc95ad
jjhiggz <jonathan.higger@gmail.com>
2020-05-27 10:57:22 -0600

    make it better

--- js.js
+++
@@ -1,6 +0,0 @@
-function saveCaptureToDB(newCapture){
-  console.log('hello from the inside, I must have tried a thousand times')
-
-  const capturesUrl = 'http://localhost:3000/captures'
-  fetch(capturesURL)
-}
